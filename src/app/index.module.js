6b10aa8011172bbd6adef7ed667d8881293a3bd2
bikram <bikrambasnet1@gmail.com>
2016-05-04 17:25:27 +0545

    s

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('doctor', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
